e90a0359cbbde146a219d8a4571a691e83f0efe4
Fetrus Jari <jariharianto7@gmail.com>
2024-06-28 23:13:47 +0700

    set up environment variables

--- src/navigations/BottomTabNavigation.tsx
+++ src/navigations/BottomTabNavigation.tsx
@@ -1,17 +1,62 @@
-import React from 'react'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import { Feather } from '@expo/vector-icons'
-import Home from '../screens/Home'
-import Search from '../screens/Search'
-import Favorite from '../screens/Favorite'
+// import React from 'react'
+// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+// import { Feather } from '@expo/vector-icons'
+// import Home from '../screens/Home'
+// import Search from '../screens/Search'
+// import Favorite from '../screens/Favorite'
 
-const Tab = createBottomTabNavigator()
+// const Tab = createBottomTabNavigator()
+
+// const BottomTabNavigator = (): JSX.Element => (
+//   <Tab.Navigator>
+//     <Tab.Screen
+//       name="Home"
+//       component={Home}
+//       options={{
+//         tabBarIcon: ({ color }) => (
+//           <Feather name="home" size={28} color={color} />
+//         ),
+//         headerShown: false,
+//       }}
+//     />
+//     <Tab.Screen
+//       name="Search"
+//       component={Search}
+//       options={{
+//         tabBarIcon: ({ color }) => (
+//           <Feather name="search" size={28} color={color} />
+//         ),
+//         headerShown: false,
+//       }}
+//     />
+//     <Tab.Screen
+//       name="Favorite"
+//       component={Favorite}
+//       options={{
+//         tabBarIcon: ({ color }) => (
+//           <Feather name="heart" size={28} color={color} />
+//         ),
+//         headerShown: false,
+//       }}
+//     />
+//   </Tab.Navigator>
+// )
+
+// export default BottomTabNavigator
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Feather } from '@expo/vector-icons';
+import HomeStackNavigation from './HomeStackNavigation';
+import Search from '../screens/Search';
+import Favorite from '../screens/Favorite';
+
+const Tab = createBottomTabNavigator();
 
 const BottomTabNavigator = (): JSX.Element => (
   <Tab.Navigator>
     <Tab.Screen
-      name="Home"
-      component={Home}
+      name="HomeScreen"
+      component={HomeStackNavigation}
       options={{
         tabBarIcon: ({ color }) => (
           <Feather name="home" size={28} color={color} />
@@ -40,6 +85,6 @@ const BottomTabNavigator = (): JSX.Element => (
       }}
     />
   </Tab.Navigator>
-)
+);
 
-export default BottomTabNavigator
+export default BottomTabNavigator;
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+// import React from 'react'
+// import { View, Text } from 'react-native'
 
-export default function Home(): JSX.Element {
-  return (
-    <View>
-      <Text>Home</Text>
-    </View>
-  )
-}
+// export default function Home(): JSX.Element {
+//   return (
+//     <View>
+//       <Text>Home</Text>
+//     </View>
+//   )
+// }
+import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+const Home = ({ navigation }: any): JSX.Element => (
+  <View style={styles.container}>
+    <Text>Home Screen</Text>
+    <Button
+      title="Go to Movie Detail"
+      onPress={() => navigation.navigate('MovieDetail')}
+    />
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default Home;
